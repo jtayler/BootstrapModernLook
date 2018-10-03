f16adde5c9b90d2d04a41949c468f8658cea2b37
jtayler <jtayler@oeinc.com>
2018-10-03 12:24:35 -0400

    first pass new UI frameworks

---
+++ Sources/your/app/BootsrapModernLook.java
@@ -0,0 +1,25 @@
+package your.app;
+
+import er.extensions.ERXFrameworkPrincipal;
+
+public class BootsrapModernLook extends ERXFrameworkPrincipal {
+	protected static BootsrapModernLook sharedInstance;
+	@SuppressWarnings("unchecked")
+	public final static Class<? extends ERXFrameworkPrincipal> REQUIRES[] = new Class[] {};
+
+	static {
+		setUpFrameworkPrincipalClass(BootsrapModernLook.class);
+	}
+
+	public static BootsrapModernLook sharedInstance() {
+		if (sharedInstance == null) {
+			sharedInstance = sharedInstance(BootsrapModernLook.class);
+		}
+		return sharedInstance;
+	}
+
+	@Override
+	public void finishInitialization() {
+		log.debug("BootsrapModernLook loaded");
+	}
+}
