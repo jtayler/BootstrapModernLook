562281728749c6f7191f7ef63f6062a4284d0b62
jtayler <jtayler@oeinc.com>
2018-10-05 14:48:57 -0400

    adding component

---
+++ Sources/BootsrapModernRepetition.java
@@ -0,0 +1,15 @@
+
+import com.webobjects.appserver.WOContext;
+
+import er.modern.directtoweb.components.repetitions.ERMDSimpleListPageRepetition;
+
+public class BootsrapModernRepetition extends ERMDSimpleListPageRepetition {
+
+	private static final long serialVersionUID = 1;
+
+	public BootsrapModernRepetition(WOContext context) {
+        super(context);
+    }
+	
+	
+}
