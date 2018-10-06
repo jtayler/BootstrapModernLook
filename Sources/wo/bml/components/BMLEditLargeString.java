751ca100e2d3ff414855ecfd31bbd6a7e9b99a48
jtayler <jtayler@oeinc.com>
2018-10-06 12:52:34 -0400

    progress on interface

---
+++ Sources/wo/bml/components/BMLEditLargeString.java
@@ -0,0 +1,16 @@
+package wo.bml.components;
+
+import com.webobjects.appserver.WOContext;
+
+import er.directtoweb.components.strings.ERD2WEditLargeString;
+
+public class BMLEditLargeString extends ERD2WEditLargeString {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BMLEditLargeString(WOContext context) {
+        super(context);
+    }
+}
