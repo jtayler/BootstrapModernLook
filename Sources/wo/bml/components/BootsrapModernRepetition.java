aa0879abc25dedfe7150e7b8d5d203f7fd828126
jtayler <jtayler@oeinc.com>
2018-10-06 08:32:39 -0400

    forgotten files

---
+++ Sources/wo/bml/components/BootsrapModernRepetition.java
@@ -0,0 +1,16 @@
+package wo.bml.components;
+
+import com.webobjects.appserver.WOContext;
+
+import er.modern.directtoweb.components.repetitions.ERMDSimpleListPageRepetition;
+
+public class BootsrapModernRepetition extends ERMDSimpleListPageRepetition {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BootsrapModernRepetition(WOContext context) {
+        super(context);
+    }
+}
