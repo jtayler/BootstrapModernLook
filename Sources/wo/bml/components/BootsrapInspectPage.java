a3a1bd22f68bff6b1150774579cbdea24ee16d58
jtayler <jtayler@oeinc.com>
2018-10-10 11:31:13 -0400

    progress on components, edit page is starting to show new buttons etc

---
+++ Sources/wo/bml/components/BootsrapInspectPage.java
@@ -0,0 +1,16 @@
+package wo.bml.components;
+
+import com.webobjects.appserver.WOContext;
+
+import er.modern.look.pages.*;
+
+public class BootsrapInspectPage extends ERMODInspectPage {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BootsrapInspectPage(WOContext context) {
+        super(context);
+    }
+}
