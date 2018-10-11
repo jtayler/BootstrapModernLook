5671e5b3721cc76223ce72a0b024fb827cffc240
jtayler <jtayler@oeinc.com>
2018-10-11 11:46:57 -0400

    adding a batch navigation component for shards/bootstrap

---
+++ Sources/wo/bml/components/BMLBatchNavigationBar.java
@@ -0,0 +1,16 @@
+package wo.bml.components;
+
+import com.webobjects.appserver.WOContext;
+
+import er.modern.directtoweb.components.ERMD2WBatchNavigationBar;
+
+public class BMLBatchNavigationBar extends ERMD2WBatchNavigationBar {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BMLBatchNavigationBar(WOContext context) {
+        super(context);
+    }
+}
