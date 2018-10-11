4ab3260473925d895e2a6af8c5b774a1c0ff3291
jtayler <jtayler@oeinc.com>
2018-10-11 14:34:52 -0400

    second pass at Card component

--- Sources/wo/bml/components/BMLCard.java
+++ Sources/wo/bml/components/BMLCard.java
@@ -1,7 +1,9 @@
 package wo.bml.components;
 
 import com.webobjects.appserver.WOContext;
+import com.webobjects.directtoweb.D2WContext;
 import com.webobjects.directtoweb.D2WCustomComponent;
+import com.webobjects.foundation.NSDictionary;
 
 public class BMLCard extends D2WCustomComponent {
     /**
@@ -12,4 +14,31 @@ public class BMLCard extends D2WCustomComponent {
 	public BMLCard(WOContext context) {
         super(context);
     }
+	
+	public String cardHeader() {
+		return valueForD2WKey("card-header");
+	}
+
+	public String cardImageSrc() {
+		return valueForD2WKey("card-img-top");
+	}
+
+	public String cardTitle() {
+		return valueForD2WKey("card-title");
+	}
+	
+	public String cardLink() {
+		return valueForD2WKey("card-link");
+	}
+	
+	public String cardText() {
+		return valueForD2WKey("card-text");
+	}
+
+	public String valueForD2WKey(String d2wKey) {
+		D2WContext ctx = d2wContext();
+		NSDictionary<String, String> cardSectionsContents=(NSDictionary<String, String>) ctx.valueForKey("cardSectionsContents");
+		String key = (String) cardSectionsContents.valueForKey(d2wKey);
+	    return (String) object().valueForKeyPath(key);
+	}
 }
