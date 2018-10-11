d18a707062b198d4ceee8f6e1bf7b68777570132
jtayler <jtayler@oeinc.com>
2018-10-11 12:44:38 -0400

    first pass at Card component

---
+++ Sources/wo/bml/components/BMLCard.java
@@ -0,0 +1,15 @@
+package wo.bml.components;
+
+import com.webobjects.appserver.WOContext;
+import com.webobjects.directtoweb.D2WCustomComponent;
+
+public class BMLCard extends D2WCustomComponent {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BMLCard(WOContext context) {
+        super(context);
+    }
+}
