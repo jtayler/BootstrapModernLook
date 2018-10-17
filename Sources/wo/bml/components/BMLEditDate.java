32e459a4e576f456bcfb904f6e8c354999669ab4
jtayler <jtayler@oeinc.com>
2018-10-16 21:03:26 -0400

    more ui

---
+++ Sources/wo/bml/components/BMLEditDate.java
@@ -0,0 +1,16 @@
+package wo.bml.components;
+
+import com.webobjects.appserver.WOContext;
+
+import er.directtoweb.components.dates.ERDEditDateJavascript;
+
+public class BMLEditDate extends ERDEditDateJavascript {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BMLEditDate(WOContext context) {
+        super(context);
+    }
+}
---
+++ Sources/wo/bml/components/BMLEmptyListMessage.java
@@ -0,0 +1,16 @@
+package wo.bml.components;
+
+import com.webobjects.appserver.WOContext;
+
+import er.modern.directtoweb.components.ERMDEmptyListMessage;
+
+public class BMLEmptyListMessage extends ERMDEmptyListMessage {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BMLEmptyListMessage(WOContext context) {
+        super(context);
+    }
+}
