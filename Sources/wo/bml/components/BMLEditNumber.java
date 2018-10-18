13a27356c36137658643a5f71ec7680676724f5b
jtayler <jtayler@oeinc.com>
2018-10-18 15:45:43 -0400

    more progress

---
+++ Sources/wo/bml/components/BMLEditNumber.java
@@ -0,0 +1,16 @@
+package wo.bml.components;
+
+import com.webobjects.appserver.WOContext;
+
+import er.directtoweb.components.numbers.ERD2WEditNumber;
+
+public class BMLEditNumber extends ERD2WEditNumber {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BMLEditNumber(WOContext context) {
+        super(context);
+    }
+}
