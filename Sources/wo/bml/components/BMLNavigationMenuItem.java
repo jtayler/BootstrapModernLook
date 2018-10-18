d99781bb04548292aaad6057bc6c9b3d49cf24eb
jtayler <jtayler@oeinc.com>
2018-10-18 11:52:22 -0400

    udpates

--- Sources/wo/bml/components/BMLNavigationMenuItem.java
+++ Sources/wo/bml/components/BMLNavigationMenuItem.java
@@ -2,18 +2,41 @@ package wo.bml.components;
 
 import com.webobjects.appserver.WOContext;
 import com.webobjects.foundation.NSArray;
+
+import er.extensions.appserver.navigation.ERXNavigationMenu;
 import er.extensions.appserver.navigation.ERXNavigationMenuItem;
 
 public class BMLNavigationMenuItem extends ERXNavigationMenuItem {
     /**
-	 * 
+	 *  
 	 */
 	private static final long serialVersionUID = 1L;
-
-	public NSArray items;
 	
 	public BMLNavigationMenuItem(WOContext context) {
         super(context);
     }
+	
+    public String linkClass() {
+        if(level() == 0) {
+            return "";
+        }
+        BMLNavigationMenu parent = (BMLNavigationMenu) parent();
+        boolean isSelected = isSelected();
+        boolean isDisabled = isDisabled();
+        boolean show = parent.showLevel2();
+        String linkstring = "";
+        if (show) {
+            linkstring = "nav-link nav-link" + level() + (isSelected ? " active" : (isDisabled ? " disabled" : "")) + (items().count() == 0 ? "" : " dropdown-toggle");
+        } else {
+            linkstring = "nav-link nav-link" + level() + (isSelected ? " active" : (isDisabled ? " disabled" : ""));
+        }
+        return linkstring;
+    }
+
+    public NSArray<?> items() {
+    	NSArray<?> items = ((BMLNavigationMenu) parent()).level2Items();
+        return items;
+    }
+
 	
 }
