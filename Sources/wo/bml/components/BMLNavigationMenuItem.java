bf0e1181b874aed25b976778ab363ecf3692c331
jtayler <jtayler@oeinc.com>
2018-10-19 14:26:36 -0400

    fix home button active indication, second pass at wizard

--- Sources/wo/bml/components/BMLNavigationMenuItem.java
+++ Sources/wo/bml/components/BMLNavigationMenuItem.java
@@ -1,9 +1,7 @@
 package wo.bml.components;
 
 import com.webobjects.appserver.WOContext;
-import com.webobjects.foundation.NSArray;
 
-import er.extensions.appserver.navigation.ERXNavigationMenu;
 import er.extensions.appserver.navigation.ERXNavigationMenuItem;
 
 public class BMLNavigationMenuItem extends ERXNavigationMenuItem {
@@ -23,12 +21,25 @@ public class BMLNavigationMenuItem extends ERXNavigationMenuItem {
             return "";
         }
         BMLNavigationMenu parent = (BMLNavigationMenu) parent();
+        boolean show = parent.showLevel2();
+        String linkstring = linkNoDropClass();
+        if (show) {
+            linkstring = "dropdown-toggle " + linkstring;
+        }
+        return linkstring;
+    }
+
+    public String linkNoDropClass() {
+        if(level() == 0) {
+            return "";
+        }
+        BMLNavigationMenu parent = (BMLNavigationMenu) parent();
         boolean isSelected = parent.isSelectedState(this.navigationItem());
         boolean isDisabled = isDisabled();
         boolean show = parent.showLevel2();
         String linkstring = "";
         if (show) {
-            linkstring = "dropdown-toggle nav-link nav-link-" + level() + (isSelected ? " active" : (isDisabled ? " disabled" : ""));
+            linkstring = "nav-link nav-link-" + level() + (isSelected ? " active" : (isDisabled ? " disabled" : ""));
         } else {
             linkstring = "nav-link nav-link-" + level() + (isSelected ? " active" : (isDisabled ? " disabled" : ""));
         }
