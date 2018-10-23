f3e60ee0a4f8a607d27cf1bc88176ebf5cd067ab
jtayler <jtayler@oeinc.com>
2018-10-23 19:26:53 -0400

    adding editor for relationships

---
+++ Sources/wo/bml/components/BMLEditToOne.java
@@ -0,0 +1,16 @@
+package wo.bml.components;
+
+import com.webobjects.appserver.WOContext;
+
+import er.modern.directtoweb.components.relationships.ERMD2WEditToOneTypeAhead;
+
+public class BMLEditToOne extends ERMD2WEditToOneTypeAhead {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BMLEditToOne(WOContext context) {
+        super(context);
+    }
+}
