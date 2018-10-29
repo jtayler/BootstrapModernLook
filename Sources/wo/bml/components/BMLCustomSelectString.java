15a68e9b1889e6257c5573c9a7f19030cc75fa27
jtayler <jtayler@oeinc.com>
2018-10-29 13:00:08 -0400

    adding a simple popup that sets text strings based on rule simple arrays

---
+++ Sources/wo/bml/components/BMLCustomSelectString.java
@@ -0,0 +1,32 @@
+package wo.bml.components;
+
+import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.WORequest;
+
+import er.directtoweb.components.strings.ERD2WEditString;
+
+public class BMLCustomSelectString extends ERD2WEditString {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String string;
+	public Integer index;
+	
+	public BMLCustomSelectString(WOContext context) {
+        super(context);
+    }
+	
+	public boolean isSelected() {
+		String value = (String)value();
+		boolean selected = string.equalsIgnoreCase(value);
+		return selected;
+	}
+	
+	public String selected() {
+		String selected = isSelected() ? "selected":"";
+		return selected;
+	}
+		
+}
