f10ff25b069693ff9cd4b6c5311721e64245596e
jtayler <jtayler@oeinc.com>
2018-10-31 22:12:13 -0400

    first pass input group wrapper continued progress ui

---
+++ Sources/wo/bml/components/BMLInputGroupWrapper.java
@@ -0,0 +1,16 @@
+package wo.bml.components;
+
+import com.webobjects.appserver.WOContext;
+
+import er.directtoweb.components.ERD2WStatelessComponent;
+
+public class BMLInputGroupWrapper extends ERD2WStatelessComponent {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BMLInputGroupWrapper(WOContext context) {
+        super(context);
+    }
+}
