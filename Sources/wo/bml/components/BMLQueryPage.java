8219ec08f28d73d45f7afcd53fa7de6c36b7bdc2
Jesse Tayler <jtayler@oeinc.com>
2021-05-16 12:09:10 -0400

    adding some stuff for query pages and improving smart button control

--- Sources/wo/bml/components/BMLQueryPage.java
+++ Sources/wo/bml/components/BMLQueryPage.java
@@ -1,8 +1,13 @@
 package wo.bml.components;
 
+import com.webobjects.appserver.WOComponent;
 import com.webobjects.appserver.WOContext;
+import com.webobjects.directtoweb.D2WPage;
+import com.webobjects.directtoweb.ERD2WUtilities;
 
+import er.extensions.foundation.ERXValueUtilities;
 import er.modern.look.pages.ERMODQueryPage;
+import er.modern.look.pages.ERMODQueryPage.Keys;
 
 public class BMLQueryPage extends ERMODQueryPage {
     /**
@@ -13,4 +18,10 @@ public class BMLQueryPage extends ERMODQueryPage {
 	public BMLQueryPage(WOContext context) {
         super(context);
     }
+	
+	public WOComponent cancelAction() {
+		//WOComponent page = super.cancelAction();
+		return returnPage;
+	}
+
 }
--- Sources/wo/bml/components/BootstrapModernRepetition.java
+++ Sources/wo/bml/components/BootstrapModernRepetition.java
@@ -1,10 +1,16 @@
 package wo.bml.components;
 
+import com.webobjects.appserver.WOActionResults;
+import com.webobjects.appserver.WOComponent;
 import com.webobjects.appserver.WOContext;
+import com.webobjects.directtoweb.D2W;
+import com.webobjects.directtoweb.EditPageInterface;
+import com.webobjects.directtoweb.NextPageDelegate;
+import com.webobjects.directtoweb.QueryPageInterface;
 
 import er.modern.directtoweb.components.repetitions.ERMDSimpleListPageRepetition;
 
-public class BootstrapModernRepetition extends ERMDSimpleListPageRepetition {
+public class BootstrapModernRepetition extends ERMDSimpleListPageRepetition implements NextPageDelegate {
     /**
 	 * 
 	 */
@@ -13,4 +19,25 @@ public class BootstrapModernRepetition extends ERMDSimpleListPageRepetition {
 	public BootstrapModernRepetition(WOContext context) {
         super(context);
     }
+	
+	public WOComponent nextPage;
+	
+	public WOActionResults smartCreateAction() {
+		nextPage = context().page();
+		 EditPageInterface nextPage = D2W.factory().editPageForNewObjectWithEntityNamed(d2wContext().entity().name(), session());
+		 nextPage.setNextPageDelegate(this);
+		return (WOActionResults) nextPage;
+	}
+
+	public WOActionResults smartFindAction() {
+		 QueryPageInterface nextPage = D2W.factory().queryPageForEntityNamed(d2wContext().entity().name(), session());
+		 //nextPage.setNextPageDelegate(this);
+		return (WOActionResults) nextPage;
+	}
+
+	@Override
+	public WOComponent nextPage(WOComponent arg0) {
+		return nextPage;
+	}
+
 }
