329e790ab8feb5afece814454321f36a09c2b261
Jesse Tayler <jtayler@oeinc.com>
2022-02-08 17:19:29 -0500

    adding span identifier

--- Sources/wo/bml/components/BMLInputGroupWrapper.java
+++ Sources/wo/bml/components/BMLInputGroupWrapper.java
@@ -13,4 +13,13 @@ public class BMLInputGroupWrapper extends ERD2WStatelessComponent {
 	public BMLInputGroupWrapper(WOContext context) {
         super(context);
     }
+
+	/**
+	 * @return the spanID
+	 */
+	public String spanID() {
+		String spanID = valueForKeyPath("d2wContext.id") + "_Span";
+		return spanID;
+	}
+
 }
