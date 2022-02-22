b91138c27a44eb29a0db91ba073af24ea97013bb
Jesse Tayler <jtayler@oeinc.com>
2022-02-21 22:36:24 -0500

    wow, a bug

--- Sources/wo/bml/components/BMLEditDate.java
+++ Sources/wo/bml/components/BMLEditDate.java
@@ -4,12 +4,13 @@ import com.webobjects.appserver.WOContext;
 import com.webobjects.directtoweb.D2WEditDate;
 
 public class BMLEditDate extends D2WEditDate {
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
 	public BMLEditDate(WOContext context) {
-        super(context);
-    }
+		super(context);
+	}
+
 }
